b981027e07cc00007b1a8503428f844826312c0f
Orso Meneghini <meneghini@fusion.gat.com>
2014-02-11 20:11:36 -0800

    * Added calculation of particles and momentum fluxes * Modified definition of some of the input quantities * Use of double precision FANN

--- onetwo/brainfuse.c
+++ onetwo/brainfuse.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include "fann.h"
+#include "doublefann.h"
 
-int run_net_on_data_(int * num_data, int * num_input, int * num_output, fann_type * input, fann_type * output, char *path, int *debug){
+int run_net_on_data_(int * num_data, int * num_input, int * num_output, double * input, double * output, char *path, int *debug){
   struct fann *ann;
   fann_type *calc_in, *calc_out;
   int i, j, k;
 
   calc_in  = (fann_type *)malloc(*num_input  * sizeof(fann_type));
-  printf("%d %d %d\n",*num_data,*num_input,*num_output);
+  printf("n=%d in=%d out=%d prec=%d\n",*num_data,*num_input,*num_output,sizeof(fann_type));
 
   //////////////////////////////
   if (debug) printf("- network from %s\n",path);
@@ -24,7 +24,7 @@ int run_net_on_data_(int * num_data, int * num_input, int * num_output, fann_typ
   for(i = 0; i < *num_data; i++){
     //load
     for(j = 0; j < *num_input; j++){
-      calc_in[j]=input[i + j * *num_data];
+      calc_in[j]=(fann_type)input[i + j * *num_data];
     }
     //scale in
     fann_scale_input(ann,calc_in);
@@ -34,15 +34,20 @@ int run_net_on_data_(int * num_data, int * num_input, int * num_output, fann_typ
     fann_descale_output(ann,calc_out);
     //save
     for(j = 0; j < *num_output; j++){
-      output[i + j * *num_data]=calc_out[j];
+      output[i + j * *num_data]=(double)calc_out[j];
     }
   }
 
-  //if(debug){
-  //  for(i = 0; i < *num_data; i++){
-  //    printf("  (%f, %f) -> %f\n", input[i], input[i+*num_data], output[i]);
-  //  }
-  //}
+  //////////////////////////////
+  if(debug>1){
+    for(i = 0; i < *num_data; i++){
+      for( j = 0; j < *num_output; j++){
+	printf("%f ",output[i + j * *num_data]);
+      }
+      printf("\n");
+    }
+  }
+  //////////////////////////////
 
   //////////////////////////////
   if (debug) printf("- cleanup\n");
@@ -53,62 +58,3 @@ int run_net_on_data_(int * num_data, int * num_input, int * num_output, fann_typ
   return 0;
 }
 
-/*
-
-int hello_(char *cc, int ll){
-  cc[ll--] = '\0';  // NULL terminate the string
-  printf("hello %s\n",cc);
-  return 0;
-}
-
-int main1(){
-  int ret;
-  unsigned int i;
-  int num_data, num_input, num_output;
-
-  //////////////////////////////
-  printf("DATA\n");
-  //////////////////////////////
-  num_data=4;
-  num_input=2;
-  num_output=1;
-  fann_type ** input  = (fann_type **) calloc(num_data, sizeof(fann_type *));
-  fann_type ** output = (fann_type **) calloc(num_data, sizeof(fann_type *));
-  for (i = 0; i < num_data; i++){
-    input[i]  = (fann_type *)malloc(num_input  * sizeof(fann_type));
-    output[i] = (fann_type *)malloc(num_output * sizeof(fann_type));
-  }
-
-  input[0][0]=-1;
-  input[0][1]=-1;
-  output[0][0]=-1;
-
-  input[1][0]=-1;
-  input[1][1]=1;
-  output[1][0]=1;
-
-  input[2][0]=1;
-  input[2][1]=-1;
-  output[2][0]=1;
-
-  input[3][0]=1;
-  input[3][1]=1;
-  output[3][0]=-1;
-
-
-  //////////////////////////////
-  printf("CALL\n");
-  //////////////////////////////
-  run_net_on_data_(num_data,num_input,num_output,input,output);
-
-  //////////////////////////////
-  printf("CHECK\n");
-  //////////////////////////////
-  for(i = 0; i < num_data; i++){
-    printf("XOR test (%f, %f) -> %f\n", input[i][0], input[i][1], output[i][0]);
-  }
-
-  return ret;
-}
-
-*/
--- onetwo/brainfuse_mod.f90
+++ onetwo/brainfuse_mod.f90
@@ -32,12 +32,12 @@ CONTAINS
   END SUBROUTINE GRADIENT
 !========================
 !========================
-  SUBROUTINE brainfuse(nn, bt, ip, r, rmaj, kappa, ne, ni, te, ti, q, vol, wt, press, Qe, Qi)
+  SUBROUTINE brainfuse(nn, bt, ip, r, rmaj, kappa, ne, ni, te, ti, q, vol, wt, press, Qe, Qi, Ge, Gi, Pr)
     IMPLICIT NONE
-    INTEGER*4 debug, nn, j, i, dummy
+    INTEGER*4 debug, nn, j, i, dummy, num_extra
     PARAMETER (debug=1)
-    REAL*8  bt, ip, r(nn), rmaj(nn), kappa(nn), delta(nn), ne(nn), ni(nn), te(nn), ti(nn), q(nn), vol(nn), wt(nn), press(nn), Qe(nn), Qi(nn)
-    REAL*8, DIMENSION (:), ALLOCATABLE :: cs, bunit, dte, dti, dne, dni, dr, dq, dkappa, ddelta, dvol, dwt, rhos, dpress
+    REAL*8  bt, ip, r(nn), rmaj(nn), kappa(nn), delta(nn), ne(nn), ni(nn), te(nn), ti(nn), q(nn), vol(nn), wt(nn), press(nn), Qe(nn), Qi(nn), Ge(nn), Gi(nn), Pr(nn)
+    REAL*8, DIMENSION (:), ALLOCATABLE :: cs, bunit, dte, dti, dne, dni, dr, dq, dkappa, ddelta, dvol, dwt, rg, dpress
     REAL*8 a, mD, eV, pi
     PARAMETER (mD = 3.3475E-27)
     PARAMETER (eV = 1.60217646E-19)
@@ -47,29 +47,15 @@ CONTAINS
     CHARACTER input_names(50)*255
     CHARACTER output_names(50)*255
     CHARACTER dbgtbl_names(100)*255
-    REAL*4, DIMENSION (:,:), ALLOCATABLE :: input, output, dbgtbl
+    REAL*8, DIMENSION (:,:), ALLOCATABLE :: input, output, dbgtbl
     INTEGER num_input, num_output
 
 !========================
 
     IF (debug) WRITE(*,*)'Running brainfuse!'
 
-    num_input=6
-    num_output=2
-    input_names(1)='rmin_loc'
-    input_names(2)='rmaj_loc'
-    input_names(3)='kappa'
-    input_names(4)='taus'
-    input_names(5)='as'
-    input_names(6)='q'
-    output_names(1)='Qe'
-    output_names(2)='Qi'
-    slogi = 0
-    slogo = 0
-    logo  = 0
-
     num_input=19
-    num_output=2
+    num_output=5
     input_names(1 )='RMIN_LOC'
     input_names(2 )='RMAJ_LOC'
     input_names(3 )='kappa'
@@ -89,25 +75,28 @@ CONTAINS
     input_names(17)='q'
     input_names(18)='Q_PRIME_LOC'
     input_names(19)='RHOS'
-    output_names(1)='Qe_Qnorm'
-    output_names(2)='Qi_Qnorm'
-    slogi = 1
-    slogo = 1
+    output_names(1)='Qe_norm'
+    output_names(2)='Qi_norm'
+    output_names(3)='Ge_norm'
+    output_names(4)='Gi_norm'
+    output_names(5)='Pr_norm'
+    slogi = 0
+    slogo = 0
     logo  = 0
 
 !========================
 
     a=r(nn)
-    q=ABS(q)
+    !q=q*SIGN(Ip*0+1,Ip)*SIGN(Bt*0+1,Bt)
 
     ALLOCATE( cs(nn) )
     cs = SQRT(1E3*eV*te/mD)
 
-    ALLOCATE( rhos(nn) )
-    rhos=SQRT(2.*1E3*eV*ti*mD)/(eV*ABS(Bt))/a
+    ALLOCATE( rg(nn) )
+    rg=mD*SQRT(2.*1E3*eV*ti/mD)/(eV*ABS(Bt))
 
     ALLOCATE( bunit(nn) )
-    bunit = cs*mD/(eV*rhos*a)
+    bunit = cs*mD/(eV*rg)
 
     ALLOCATE( dr(nn) )
     call GRADIENT(nn,r,dr)
@@ -227,19 +216,19 @@ CONTAINS
        CASE('gamma_e')
           input(:,j) = -dwt*r/q                !gamma E
        CASE('vpar')
-          input(:,j) = -SIGN(Ip*0+1,Ip)*rmaj*wt/cs  !vpar
+          input(:,j) = -SIGN(Ip*0+1,Ip)*rmaj*wt/cs       !vpar
        CASE('vpar_shear')
-          input(:,j) = -SIGN(Ip*0+1,Ip)*(-rmaj*dwt)*r/cs  !vpar shear
+          input(:,j) = -SIGN(Ip*0+1,Ip)*(-rmaj*dwt)*r/cs !vpar shear
        CASE('vexb_shear')
-          input(:,j) = -SIGN(Ip*0+1,Ip)*(-dwt*r/q)*r/cs   !vper shear
+          input(:,j) = -SIGN(Ip*0+1,Ip)*(-dwt*r/q)*r/cs  !vper shear
        CASE('betae')
-          input(:,j) = 8*pi*ne*te/bunit**2     !electron beta
+          input(:,j) = 8*pi*ne*te/bunit**2               !electron beta
        CASE('xnue')
           input(:,j) = 1.33E5*(ne/1E20)/te**1.5*a/cs     !normalized ei collisionality
        CASE('debye')
-          input(:,j) = 2.35E-5*SQRT(te/(ne/1E20))/(rhos*a)     !normalized Debye length
+          input(:,j) = 2.35E-5*SQRT(te/(ne/1E20))/rg     !normalized Debye length
        CASE('qgb')
-          input(:,j) = ne*cs*(te*1E3*eV)*(rhos/a)**2     !gyrobohm flux
+          input(:,j) = ne*cs*(te*1E3*eV)*rg**2           !gyrobohm flux
        CASE('lte','rlts_1')
           input(:,j) = -a*dte/te               !electron temperature scale length
        CASE('lti','rlts_2')
@@ -251,7 +240,9 @@ CONTAINS
        CASE('p_prime_loc','lp','rlps')
           input(:,j) = -a*dpress/press         !total pressure scale length
        CASE('rhos')
-          input(:,j) = rhos                    !rho
+          input(:,j) = rg/a                    !rhostar
+       CASE('rhos_loc')
+          input(:,j) = rg/r                    !local rhostar
        CASE DEFAULT
           dummy=0
        END SELECT
@@ -265,17 +256,25 @@ CONTAINS
 !========================
     
     DO j=1,num_input
-       IF (slogi) input(:,j)=SIGN(input(:,j)*0+1,input(:,j))*(LOG10(ABS(input(:,j))+1)-ALOG10(1.))
+       IF (slogi.eq.1) THEN
+          input(:,j)=SIGN(input(:,j)*0+1,input(:,j))*(LOG10(ABS(input(:,j))+1)-ALOG10(1.))
+       ENDIF
     ENDDO
 
     CALL run_net_on_data(nn, num_input, num_output, input, output, trim(brainfuse_path)//CHAR(0), debug)
 
     DO j=1,num_input
-       IF (slogi) input(:,j)=SIGN(input(:,j)*0+1,input(:,j))*(10**(ABS(input(:,j))+LOG10(1.))-1)
+       IF (slogi.eq.1) THEN
+          input(:,j)=SIGN(input(:,j)*0+1,input(:,j))*(10**(ABS(input(:,j))+LOG10(1.))-1)
+       ENDIF
     ENDDO
     DO j=1,num_output
-       IF (slogo) output(:,j)=SIGN(output(:,j)*0+1,output(:,j))*(10**(ABS(output(:,j))+LOG10(1.))-1)
-       IF (logo)  output(:,j)=10**output(:,j)
+       IF (slogo.eq.1) THEN
+          output(:,j)=SIGN(output(:,j)*0+1,output(:,j))*(10**(ABS(output(:,j))+LOG10(1.))-1)
+       ENDIF
+       IF (logo) THEN 
+          output(:,j)=10**output(:,j)
+       ENDIF
     ENDDO
 
 !========================
@@ -283,14 +282,32 @@ CONTAINS
     DO j=1,num_output
        dummy=1
        SELECT CASE (to_lower(trim(output_names(j))))
+
        CASE('qe')
           Qe=output(:,j)
-       CASE('qe_qnorm')
+       CASE('qe_norm')
           Qe=output(:,j)*dvol
+
        CASE('qi')
           Qi=output(:,j)
-       CASE('qi_qnorm')
+       CASE('qi_norm')
           Qi=output(:,j)*dvol
+
+       CASE('ge')
+          Ge=output(:,j)
+       CASE('ge_norm')
+          Ge=output(:,j)*dvol
+
+       CASE('gi')
+          Gi=output(:,j)
+       CASE('gi_norm')
+          Gi=output(:,j)*dvol
+
+       CASE('pr')
+          Pr=output(:,j)
+       CASE('pr_norm')
+          Pr=output(:,j)*dvol
+
        CASE DEFAULT
           dummy=0
        END SELECT
@@ -305,24 +322,30 @@ CONTAINS
 !========================
 
     IF (debug) THEN
+       
+       num_extra=2
+       ALLOCATE( dbgtbl(nn,num_input+num_output+num_extra) ) !input+output+dvol+bt
 
-       ALLOCATE( dbgtbl(nn,num_input+num_output+1) )
-
+       !input
        DO j=1,num_input
           dbgtbl_names(j)=input_names(j)
           dbgtbl(:,j)=input(:,j)
        ENDDO
+       !output
        DO j=1,num_output
           dbgtbl_names(num_input+j)=output_names(j)
           dbgtbl(:,num_input+j)=output(:,j)
        ENDDO
+       !extra
        dbgtbl_names(num_input+num_output+1)='dvol'
        dbgtbl(:,num_input+num_output+1)=dvol
+       dbgtbl_names(num_input+num_output+2)='Bt'
+       dbgtbl(:,num_input+num_output+2)=bt
 
        OPEN(unit=17,file='brainfuse.dat',FORM='FORMATTED',STATUS='REPLACE')
-       WRITE(17,'(100(2X,A15))')(dbgtbl_names(j),j=1,num_input+num_output+1)
+       WRITE(17,'(100(2X,A15))')(dbgtbl_names(j),j=1,num_input+num_output+num_extra)
        DO i=1,nn
-          WRITE(17,'(100(1X,e16.9))')(dbgtbl(i,j),j=1,num_input+num_output+1)
+          WRITE(17,'(100(1X,e16.9))')(dbgtbl(i,j),j=1,num_input+num_output+num_extra)
        ENDDO
        CLOSE(17)
 
@@ -333,7 +356,7 @@ CONTAINS
 !========================
 
     DEALLOCATE( cs )
-    DEALLOCATE( rhos )
+    DEALLOCATE( rg )
     DEALLOCATE( bunit )
     DEALLOCATE( dr )
     DEALLOCATE( dte )
