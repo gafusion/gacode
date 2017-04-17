a8ec78b9407c1625c31f22e577cbc673d224dda4
Arsene Stephane Tema Biwole <stephanet@fusion.gat.com>
2017-04-17 16:00:17 -0700

    Sent Input and Output data from vgen  to Harvest.

--- vgen/src/vgen.f90
+++ vgen/src/vgen.f90
@@ -18,23 +18,24 @@ program vgen
   
    integer  :: ierr
    integer :: i
-  integer :: j
-  integer :: ix
-  integer :: rotation_model 
-  real :: grad_p
-  real :: ya
-  real :: yb
-  real :: vtor_diff
-  real :: er0
-  real :: omega
-  real :: omega_deriv
-  integer :: simntheta
-  real :: cpu_tot_in, cpu_tot_out
+   integer :: j
+   integer :: ix
+   integer :: rotation_model 
+   real :: grad_p
+   real :: ya
+   real :: yb
+   real :: vtor_diff
+   real :: er0
+   real :: omega
+   real :: omega_deriv
+   integer :: simntheta
+   real :: cpu_tot_in, cpu_tot_out
    CHARACTER(LEN=65507) :: harvest_sendline
    CHARACTER(LEN=255) :: harvest_tag
    CHARACTER NUL
    PARAMETER(NUL = CHAR(0))
    CHARACTER(LEN=2) :: NUM
+ 
     
 
   real, dimension(:), allocatable :: er_exp
@@ -423,7 +424,9 @@ program vgen
      ierr=set_harvest_verbose(1)
      ierr=set_harvest_protocol('TCP'//NUL)
      ierr=set_harvest_host('localhost'//NUL)
-
+    !ierr=set_harvest_payload_dbl_array(harvest_sendline,'darr'//NUL,D,SIZE(D))
+    !ierr=set_harvest_payload_flt_array(harvest_sendline,'farr'//NUL,F,SIZE(F))
+ 
      ierr=set_harvest_payload_str(harvest_sendline,'VERSION'//NUL,'APS15_1'//NUL) !no underscore to allow different versions of the same run
 
      open(unit=1,file='input.profiles.jbs',status='replace')
@@ -445,14 +448,48 @@ program vgen
              jbs_neo(i), jbs_sauter(i), jbs_nclass(i), jbs_koh(i)
         write(*,'(6(1pe14.7,2x))') EXPRO_rho(i), pflux_sum(i), &
              jbs_neo(i), jbs_sauter(i), jbs_nclass(i), jbs_koh(i)
+             
      enddo
-     ierr=set_harvest_payload_dbl(harvest_sendline,'EXPRO_RHO'//NUL,EXPRO_rho)
-     ierr=set_harvest_payload_dbl(harvest_sendline,'PFLUX_SUM'//NUL,pflux_sum)
-     ierr=set_harvest_payload_dbl(harvest_sendline,'JBS_NEO'//NUL,jbs_neo)
-     ierr=set_harvest_payload_dbl(harvest_sendline,'JBS_SAUTER'//NUL,jbs_sauter)
-     ierr=set_harvest_payload_dbl(harvest_sendline,'JBS_NCLASS'//NUL,jbs_nclass)
-     ierr=set_harvest_payload_dbl(harvest_sendline,'JBS_KOH'//NUL,jbs_koh)
- 
+
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_RHO'//NUL,EXPRO_rho,EXPRO_n_exp)
+     ierr=set_harvest_payload_int(harvest_sendline,'vel_method'//NUL,vel_method)
+     ierr=set_harvest_payload_int(harvest_sendline,'erspecies_indx'//NUL,erspecies_indx)
+     ierr=set_harvest_payload_int(harvest_sendline,'nth_min'//NUL,nth_min)
+     ierr=set_harvest_payload_int(harvest_sendline,'nth_max'//NUL,nth_max)
+!    ierr=set_harvest_payload_dbl_array(harvest_sendline,'PFLUX_SUM'//NUL,pflux_sum,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'JBS_NEO'//NUL,jbs_neo,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'JBS_SAUTER'//NUL,jbs_sauter,EXPRO_n_exp)
+!    ierr=set_harvest_payload_dbl_array(harvest_sendline,'JBS_NCLASS'//NUL,jbs_nclass,EXPRO_n_exp)
+!    ierr=set_harvest_payload_dbl_array(harvest_sendline,'JBS_KOH'//NUL,jbs_koh,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'KY_SPECTRUM'//NUL,jbs_neo,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_rmin'//NUL,EXPRO_rmin,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_rmaj'//NUL,EXPRO_rmaj,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_q'//NUL,EXPRO_q,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_kappa'//NUL,EXPRO_kappa,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_delta'//NUL,EXPRO_delta,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_z_eff'//NUL,EXPRO_z_eff,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_w0'//NUL,EXPRO_w0,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_flow_mom'//NUL,EXPRO_rmin,EXPRO_flow_mom)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_sbcx'//NUL,EXPRO_sbcx,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_sbeame'//NUL,EXPRO_sbeame,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_sscxl'//NUL,EXPRO_sscxl,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_e'//NUL,EXPRO_pow_e,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_i'//NUL,EXPRO_pow_i,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_ei'//NUL,EXPRO_pow_ei,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_zeta'//NUL,EXPRO_zeta,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_flow_beam'//NUL,EXPRO_flow_beam,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_flow_wall'//NUL,EXPRO_flow_wall,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_zmag'//NUL,EXPRO_zmag,EXPRO_n_exp)                                    
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_ptot'//NUL,EXPRO_ptot,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_polflux'//NUL,EXPRO_polflux,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_e_fus'//NUL,EXPRO_pow_e_fus,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_i_fus'//NUL,EXPRO_pow_i_fus,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_e_sync'//NUL,EXPRO_pow_e_sync,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_e_brem'//NUL,EXPRO_pow_e_brem,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_e_line'//NUL,EXPRO_pow_e_line,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_e_aux'//NUL,EXPRO_pow_e_aux,EXPRO_n_exp)
+     ierr=set_harvest_payload_dbl_array(harvest_sendline,'EXPRO_pow_i_aux'//NUL,EXPRO_pow_i_aux,EXPRO_n_exp)
+     
      close(1)
      !----------------------------------------------------------------------
 
