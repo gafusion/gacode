cb1243ccbc2564f30ecd0b045c8fe7d42492fb05
Igor Sfiligoi <igor.sfiligoi@ga.com>
2018-09-19 16:33:49 -0700

    Add restart_magic constant

--- cgyro/src/cgyro_globals.F90
+++ cgyro/src/cgyro_globals.F90
@@ -237,6 +237,7 @@ module cgyro_globals
   character(len=8) :: fmt='(I2.2)'
   character(len=6), dimension(100) :: rtag
   integer, parameter :: restart_header_size = 1024
+  integer, parameter :: restart_magic = 140906808
   !
   ! error checking
   integer :: error_status = 0
@@ -457,5 +458,5 @@ module cgyro_globals
   !---------------------------------------------------------------
 
   real :: total_memory
-  
+
 end module cgyro_globals
