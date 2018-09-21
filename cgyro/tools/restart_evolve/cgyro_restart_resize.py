3660934cb2731ac32a06ef649fe3bc49edd4bfab
Igor Sfiligoi <igor.sfiligoi@ga.com>
2018-09-21 14:25:49 -0700

    Add restart_upgrade.py

--- cgyro/tools/restart_evolve/cgyro_restart_resize.py
+++ cgyro/tools/restart_evolve/cgyro_restart_resize.py
@@ -4,6 +4,8 @@
 import sys,os
 import struct
 
+header_size = 1024
+restart_v1_fname="out.cgyro.restart"
 restart_fname="bin.cgyro.restart"
 
 class CGyroGrid:
@@ -43,6 +45,8 @@ class CGyroRestartHeader:
     def __init__(self):
         self.grid = CGyroGrid()
         self.n_species = 0
+        self.mpi_rank_order = 0
+        self.n_proc = 0
 
     def load(self,fdir):
         fname = os.path.join(fdir,restart_fname)
@@ -62,11 +66,67 @@ class CGyroRestartHeader:
              self.grid.n_xi,self.grid.n_energy,
              self.grid.n_toroidal] = struct.unpack('i12xi12xi12xi12xi12xi12x',grid_b)
             
+            mpi_b = fd.read(2*(4+12))
+            [self.mpi_rank_order,self.n_proc] = struct.unpack('i12xi12x',mpi_b)
 
+            magic_b = fd.read(4+12)
+            [magic] = struct.unpack('i12x',magic_b)
+            if (magic!=140906808):
+                raise IOError("Wrong CGyroRestartHeader magic(2) number %i"%magic)
+
+    def savev2(self,fdir):
+        fname = os.path.join(fdir,restart_fname)
+        with open(fname,"wb+") as fd:
+            magic_b= struct.pack('4i',140906808,0,0,0)
+            fd.write(magic_b)
+            version_b= struct.pack('4i',2,0,0,0)
+            fd.write(version_b)
+            gridarr=[self.grid.n_theta,self.grid.n_radial,
+                     self.n_species,
+                     self.grid.n_xi,self.grid.n_energy,
+                     self.grid.n_toroidal,
+                     self.mpi_rank_order,self.n_proc]
+            for g in gridarr:
+                g_b = struct.pack('4i',g,0,0,0)
+                fd.write(g_b)
+            fd.write(magic_b)
+
+# mpi_rank_order == 1
+def upgrade_v1v2_ro1(org_dir, new_dir, grid, n_species, n_proc):
+    org_fname = os.path.join(org_dir,restart_v1_fname)
+    new_fname = os.path.join(new_dir,restart_fname)
+            
+    # Data structure (in fortran terms)
+    # double h_x[nc,n_species,nvg,2*n_toroidal]
+
+    el_size = 8 # Double precision
+    nc =      grid.get_nc()
+    ncbytes = nc*el_size
+    nv =     grid.get_nvg()*n_species
+
+    tor2 =    grid.n_toroidal*2
+
+    zerobuf = bytearray(header_size)
+    for i in range(header_size):
+        zerobuf[i] = 0 
+
+    with open(org_fname,"rb") as org_fd:
+        with  open(new_fname,"wb") as new_fd:
+            new_fd.write(zerobuf) # dummy header
+
+            for t in range(tor2*nv):
+                tmp=org_fd.read(ncbytes)
+                new_fd.write(tmp)
+
+    # update the header
+    new_header = CGyroRestartHeader()
+    new_header.grid = grid
+    new_header.n_species = species
+    new_header.mpi_rank_order = 1
+    new_header.n_proc = n_proc
+    new_header.savev2(new_dir)
 
 def add_species(org_dir, new_dir, grid, org_pre_species, org_post_species, new_species):
-    header_size = 1024
-
     org_fname = os.path.join(org_dir,restart_fname)
     new_fname = os.path.join(new_dir,restart_fname)
 
