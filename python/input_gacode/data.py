0897f59c8d10cf3d98d9eecd2b869bcccf155ecc
Sterling Smith <smithsp@fusion.gat.com>
2019-06-10 08:44:16 -0700

    Fix python2 to python3 compatibility of ExproData class

--- python/input_gacode/data.py
+++ python/input_gacode/data.py
@@ -1,5 +1,18 @@
 from gacode import expro
 import numpy
+import sys
+if sys.version_info < (3, 0):
+    def b2s(obj):
+        return obj
+else:
+    def b2s(obj):
+        if isinstance(obj, bytes):
+            return obj.decode("utf-8")
+        import numpy
+        if isinstance(obj, numpy.ndarray) and obj.dtype.name.startswith('bytes'):
+            return numpy.reshape(numpy.array(list(map(b2s, obj.flat))), obj.shape)
+        else:
+            return obj
 
 class ExproData(dict):
 
@@ -11,9 +24,6 @@ class ExproData(dict):
                     self[item[len('expro_'):-len('str_')]] = getattr(expro, item[:-len('str_')])
                 except Exception as _excp:
                     print('WARNING: issue reading %s: %s' % (item, repr(_excp)))
-        for item in ['name', 'type']:
-            self[item] = map(lambda x: ''.join(x).strip().replace('[', '').replace(']', ''),
-                             numpy.split(numpy.array(list(self[item].tolist())), self['n_ion']))
 
         if input_profiles_compatibility_mode:
 
@@ -28,7 +38,7 @@ class ExproData(dict):
                     del self[quantity]
             self['IONS'] = []
             for k in range(self['n_ion']):
-                self['IONS'].append([self['name'][k], self['z'][k], self['mass'][k], self['type'][k]])
+                self['IONS'].append([b2s(self['name'][k]).strip(), self['z'][k], self['mass'][k], b2s(self['type'][k]).strip()])
             del self['z']
             del self['mass']
             del self['ze']
