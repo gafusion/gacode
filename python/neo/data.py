4f7830818eed3323e89893a7c0065d493f9097af
Sterling Smith <smithsp@fusion.gat.com>
2020-04-01 11:55:11 -0700

    python3:NEO:Update division to use integer division

--- python/neo/data.py
+++ python/neo/data.py
@@ -366,7 +366,7 @@ class NEOData:
         theta = self.grid['theta']
         
         
-        m_theta = (n_theta-1)/2-1
+        m_theta = (n_theta-1)//2-1
 
         data = data.reshape(n_radial, nbspecies, 6, m_theta+1)
 
