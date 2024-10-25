e2bd3193126192c5fa084971b5e0e49de603ddfd
Igor Sfiligoi <isfiligoi@sdsc.edu>
2024-10-25 17:10:31 -0400

    Properly cleanup thfac_itor

--- cgyro/src/cgyro_cleanup.F90
+++ cgyro/src/cgyro_cleanup.F90
@@ -129,6 +129,10 @@ subroutine cgyro_cleanup
       ccl_del_device(source)      
      deallocate(source)
   endif
+  if(allocated(thfac_itor)) then
+      ccl_del_device(thfac_itor)
+     deallocate(thfac_itor)
+  endif
   if(allocated(h0_old)) then
      ccl_del_device(h0_old)      
      deallocate(h0_old)
