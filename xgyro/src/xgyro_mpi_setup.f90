9287bf14e831fb33246f623541c84560faf6a2cb
Igor Sfiligoi <isfiligoi@sdsc.edu>
2024-11-26 14:13:29 -0500

    Add support for MPI_RANK_ORDER=2, add reg test

--- xgyro/src/xgyro_mpi_setup.f90
+++ xgyro/src/xgyro_mpi_setup.f90
@@ -18,21 +18,59 @@ subroutine xgyro_mpi_setup
 
   integer :: i_group, splitkey
   character(len=192) :: msg
-  integer :: i, tmpi
+  integer :: i, j, i_mpi
+  integer, dimension(:), allocatable :: groups
 
   ! Local group indices:
 
   if (xgyro_mpi_rank_order == 1) then
-     i_group = 1
-     tmpi = 0
+     ! convert from incremental xgyro_n_mpi(i) to abs mapping in groups
+     ! e.g.
+     !  1 -> 1
+     !  2 -> 2
+     !       3
+     !  1 -> 4
+     !
+     allocate(groups(xgyro_n_proc))
+     i_mpi = 0
      do i=1,xgyro_n_dirs
-       tmpi = tmpi + xgyro_n_mpi(i)
-       if (tmpi <= xgyro_i_proc) i_group = i_group + 1 ! we could break, but keep it simple
+       do j=1,xgyro_n_mpi(i)
+         i_mpi = i_mpi + 1 ! keep it 1-based
+         groups(i_mpi) = i
+       enddo
      enddo
+
+     ! then use the i_mpi -> group mapping
+     i_group = groups(xgyro_i_proc+1)  !xgyro_i_proc is 0-based
+
+     deallocate(groups)
      call xgyro_info('MPI rank alignment 1')
   else
-     call xgyro_error('Unsupported MPI rank alignment, not 1')
-     return
+     ! find the 
+     ! e.g.
+     !   1 -> 1 -
+     !   2 -> 2 4
+     !   1 -> 3 -
+     allocate(groups(xgyro_n_dirs))
+     do i=1,xgyro_n_dirs
+        groups(i) = xgyro_n_mpi(i) ! make a copy of xgyro_n_mpi, so we save the orig values
+     enddo
+     ! grab the first i_mpi that was not claimed by lower i
+     i_group = 0
+     do i=0,xgyro_i_proc
+       ! try to pick the next group
+       i_group = i_group+1
+       if (i_group>xgyro_n_dirs) i_group = 1 ! wraparound
+       ! if the current group is not available, find the next avaialble one
+       do while ( groups(i_group) == 0 )
+               i_group = i_group+1
+               if (i_group>xgyro_n_dirs) i_group = 1 ! wraparound
+       enddo
+       ! remember the choice
+       groups(i_group) = groups(i_group) - 1
+     enddo
+     deallocate(groups)
+     call xgyro_info('MPI rank alignment 2')
   endif
   xgyro_i_dir = i_group;
 
